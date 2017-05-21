6d78a1671a115675467506013901de2d429aa5ef
warizz <warizz.yutanan@gmail.com>
2017-05-21 12:29:05 +0700

    add 2 more threes

--- src/models/period.js
+++ src/models/period.js
@@ -1,29 +1,34 @@
 const mongoose = require('mongoose');
 
-const PeriodSchema = new mongoose.Schema({
-  bets: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Bet' }],
-  createdBy: {
-    type: String,
-    required: true,
+const PeriodSchema = new mongoose.Schema(
+  {
+    bets: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Bet' }],
+    createdBy: {
+      type: String,
+      required: true
+    },
+    endedAt: {
+      type: Date,
+      required: true
+    },
+    isOpen: {
+      default: true,
+      type: Boolean,
+      required: true
+    },
+    result: {
+      six: String,
+      two: String,
+      firstThree: String,
+      secondThree: String,
+      thirdThree: String,
+      fourthThree: String
+    }
   },
-  endedAt: {
-    type: Date,
-    required: true,
-  },
-  isOpen: {
-    default: true,
-    type: Boolean,
-    required: true,
-  },
-  result: {
-    six: String,
-    two: String,
-    firstThree: String,
-    secondThree: String,
+  {
+    timestamps: true
   }
-}, {
-  timestamps: true
-});
+);
 
 PeriodSchema.set('toJSON', { virtuals: true });
 
