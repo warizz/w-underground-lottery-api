3a0dea20de4c9e206b9318fd38ba0757d32d37a3
warizz yutanan <warizz.yutanan@gmail.com>
2017-06-02 18:38:53 +0700

    Prettier

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -26,7 +26,7 @@ function facebookProfileGetter(access_token) {
           isAdmin: res.data.isAdmin,
           name: res.data.name,
           oauth_id: res.data.id,
-          picture: res.data.picture,
+          picture: res.data.picture
         };
         resolve(user);
       })
@@ -53,26 +53,23 @@ function fakeProfileGetter(access_token) {
       oauth_id: 'awefawefaewfaf',
       picture: {
         data: {
-          url: 'https://scontent.fbkk1-4.fna.fbcdn.net/v/t1.0-9/13151495_10153419716527676_5289925877114749609_n.jpg',
-        },
-      },
+          url: 'https://scontent.fbkk1-4.fna.fbcdn.net/v/t1.0-9/13151495_10153419716527676_5289925877114749609_n.jpg'
+        }
+      }
     });
   });
 }
 
 function get(req, res) {
-  User
-    .findById(req.user_id)
-    .select('id is_admin name picture')
-    .exec((error, doc) => {
-      if (error) {
-        return res.status(400).send();
-      }
-      if (!doc) {
-        return res.status(401).send();
-      }
-      return res.status(200).json(doc);
-    });
+  User.findById(req.user_id).select('id is_admin name picture').exec((error, doc) => {
+    if (error) {
+      return res.status(400).send();
+    }
+    if (!doc) {
+      return res.status(401).send();
+    }
+    return res.status(200).json(doc);
+  });
 }
 
 function getToken(authenticator, short_lived_token) {
@@ -80,14 +77,11 @@ function getToken(authenticator, short_lived_token) {
 }
 
 function logOut(req, res, next) {
-  User
-    .findById(req.user_id)
-    .update({ $unset: { access_token: '' } })
-    .exec((error, raw) => {
-      if (error) return next(error);
-      if (raw.nModified > 0) return res.status(200).send();
-      return res.status(401).send();
-    });
+  User.findById(req.user_id).update({ $unset: { access_token: '' } }).exec((error, raw) => {
+    if (error) return next(error);
+    if (raw.nModified > 0) return res.status(200).send();
+    return res.status(401).send();
+  });
 }
 
 function post(req, res) {
@@ -103,57 +97,54 @@ function post(req, res) {
 
 function saveUserData(user_data) {
   return new Promise((resolve, reject) => {
-    User
-      .where({ oauth_id: user_data.oauth_id })
-      .findOne()
-      .exec((error, doc) => {
-        if (error) {
-          return reject(error);
-        }
-        if (doc) {
-          User
-            .findByIdAndUpdate(doc._id, { access_token: user_data.access_token }, { new: true }, (updated_error, updated_doc) => {
-              if (updated_error) {
-                return reject(updated_error);
-              }
-              return resolve(updated_doc);
-            });
-          return;
-        }
-        const allowNewUser = process.env.ALLOW_NEW_USER || false;
-        if (!allowNewUser) {
-          return reject('not allowed');
-        }
-        const user = new User();
-        user.access_token = user_data.access_token;
-        user.name = user_data.name;
-        user.oauth_id = user_data.oauth_id;
-        user.picture = user_data.picture.data.url;
-        user.save((save_error, new_doc) => {
-          if (save_error) {
-            return reject(save_error);
+    User.where({ oauth_id: user_data.oauth_id }).findOne().exec((error, doc) => {
+      if (error) {
+        return reject(error);
+      }
+      if (doc) {
+        User.findByIdAndUpdate(
+          doc._id,
+          { access_token: user_data.access_token },
+          { new: true },
+          (updated_error, updated_doc) => {
+            if (updated_error) {
+              return reject(updated_error);
+            }
+            return resolve(updated_doc);
           }
-          return resolve(new_doc);
-        });
+        );
+        return;
+      }
+      const allowNewUser = process.env.ALLOW_NEW_USER || false;
+      if (!allowNewUser) {
+        return reject('not allowed');
+      }
+      const user = new User();
+      user.access_token = user_data.access_token;
+      user.name = user_data.name;
+      user.oauth_id = user_data.oauth_id;
+      user.picture = user_data.picture.data.url;
+      user.save((save_error, new_doc) => {
+        if (save_error) {
+          return reject(save_error);
+        }
+        return resolve(new_doc);
       });
+    });
   });
 }
 
 function validateToken(access_token) {
   return new Promise((resolve, reject) => {
-    User
-      .where({ access_token })
-      .findOne()
-      .select('id')
-      .exec((error, doc) => {
-        if (error) {
-          return reject(error);
-        }
-        if (!doc) {
-          return reject('invalid token');
-        }
-        return resolve(doc.id);
-      });
+    User.where({ access_token }).findOne().select('id').exec((error, doc) => {
+      if (error) {
+        return reject(error);
+      }
+      if (!doc) {
+        return reject('invalid token');
+      }
+      return resolve(doc.id);
+    });
   });
 }
 
@@ -167,5 +158,5 @@ module.exports = {
   logOut,
   post,
   saveUserData,
-  validateToken,
+  validateToken
 };
